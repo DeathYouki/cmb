2288258dca0cfc699855f7c0ec5bd29285c874cc
Youki Sat <deathyouki@gmail.com>
2017-06-02 14:55:04 +0000

    Installed moloquent/moloquent

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Moloquent\MongodbServiceProvider::class,
+        Moloquent\Auth\PasswordResetServiceProvider::class,
+        Moloquent\Passport\PassportServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +229,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Moloquent'  => 'Moloquent\Eloquent\Model',
 
     ],
 
