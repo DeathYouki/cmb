2aeaa568a54a831b67d8509ba14858723c46b085
Youki Sat <deathyouki@gmail.com>
2017-06-09 12:17:29 +0100

    Admin have a seperate login page and landing(after he's loged) page

---
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+//use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Auth;
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+    public function __construct() {
+        $this->middleware('guest:admin');
+    }
+
+    public function showLoginForm()
+    {
+        return view('layouts.Admin.auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        //Validate the form data
+        $this->validate($request, [
+            'email' => 'required|required',
+            'password' => 'required|String|min:6'
+        ]);
+
+        //Attempt to log the admin in
+        if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->remember)){
+            //If successful then redirect to admin dashboard
+            return redirect()->intended(route('admin.home'));
+        }
+        //If unsuccessful then redirect back to login
+        return redirect()->back()->withInput($request->only('email', 'remember')); 
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,100 +14,108 @@ Route::get('/', function() {
 	return "HOME";
 });
 
-Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function() {
-	Route::get('/', function() {
-		return redirect()->route('en-suspens.index');
-	});
-	//Commandes Routes
-	Route::group(['prefix' => 'commandes'], function() {
+Route::group(['prefix' => 'admin'], function() {
+	// Admin Authentication Routes
+	$this->get('login', 'Admin\Auth\LoginController@showLoginForm')->name('admin.login');
+	$this->post('login', 'Admin\Auth\LoginController@login')->name('admin.login.submit');
+	//$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+	Route::get('/home', 'Admin\HomeController@index')->name('admin.home');
+
+	Route::group(['middleware' => 'auth:admin'], function() {
 		Route::get('/', function() {
 			return redirect()->route('en-suspens.index');
 		});
-		//Berra Routes
-		Route::group(['prefix' => 'berra'], function() {
+		//Commandes Routes
+		Route::group(['prefix' => 'commandes'], function() {
 			Route::get('/', function() {
 				return redirect()->route('en-suspens.index');
 			});
-			Route::resource('/en-suspens', 'Admin\Commandes\Berra\EnSuspensController');
-			Route::resource('/payés', 'Admin\Commandes\Berra\PayesController');
-			Route::resource('/achetés', 'Admin\Commandes\Berra\AchetesController');
-			Route::resource('/rejetés', 'Admin\Commandes\Berra\RejetesController', 
+			//Berra Routes
+			Route::group(['prefix' => 'berra'], function() {
+				Route::get('/', function() {
+					return redirect()->route('en-suspens.index');
+				});
+				Route::resource('/en-suspens', 'Admin\Commandes\Berra\EnSuspensController');
+				Route::resource('/payés', 'Admin\Commandes\Berra\PayesController');
+				Route::resource('/achetés', 'Admin\Commandes\Berra\AchetesController');
+				Route::resource('/rejetés', 'Admin\Commandes\Berra\RejetesController', 
+					['names' => [
+							'index' => 'rejetésberra.index',
+							'create' => 'rejetésberra.create',
+							'store' => 'rejetésberra.store',
+							'show' => 'rejetésberra.show',
+							'edit' => 'rejetésberra.edit',
+							'update' => 'rejetésberra.update',
+							'destroy' => 'rejetésberra.destroy'
+						]
+					]);
+			});
+			//Hna Routes
+			Route::group(['prefix' => 'hna'], function() {
+				Route::get('/', function() {
+					return redirect()->route('commandés.index');
+				});
+				Route::resource('/commandés', 'Admin\Commandes\Hna\CommandesController');
+				Route::resource('/livrés', 'Admin\Commandes\Hna\LivresController');
+				Route::resource('/rejetés', 'Admin\Commandes\Hna\RejetesController', 
+					['names' => [
+							'index' => 'rejetéshna.index',
+							'create' => 'rejetéshna.create',
+							'store' => 'rejetéshna.store',
+							'show' => 'rejetéshna.show',
+							'edit' => 'rejetéshna.edit',
+							'update' => 'rejetéshna.update',
+							'destroy' => 'rejetéshna.destroy'
+						]
+					]);		
+			});
+		});
+		//Haouta Routes
+		Route::group(['prefix' => 'haouta'], function() {
+			Route::get('/', function() {
+				return redirect()->route('vitrinehaouta.index');
+			});
+			Route::resource('/vitrine', 'Admin\Haouta\VitrineController', 
 				['names' => [
-						'index' => 'rejetésberra.index',
-						'create' => 'rejetésberra.create',
-						'store' => 'rejetésberra.store',
-						'show' => 'rejetésberra.show',
-						'edit' => 'rejetésberra.edit',
-						'update' => 'rejetésberra.update',
-						'destroy' => 'rejetésberra.destroy'
+						'index' => 'vitrinehaouta.index',
+						'create' => 'vitrinehaouta.create',
+						'store' => 'vitrinehaouta.store',
+						'show' => 'vitrinehaouta.show',
+						'edit' => 'vitrinehaouta.edit',
+						'update' => 'vitrinehaouta.update',
+						'destroy' => 'vitrinehaouta.destroy'
 					]
 				]);
+			Route::resource('/en-attentes', 'Admin\Haouta\EnAttentesController');
+			Route::resource('/ajout-lien', 'Admin\Haouta\AjoutLienController');
 		});
-		//Hna Routes
-		Route::group(['prefix' => 'hna'], function() {
+		//CMH Routes
+		Route::group(['prefix' => 'cmh'], function() {
 			Route::get('/', function() {
-				return redirect()->route('commandés.index');
+				return redirect()->route('vitrinecmh.index');
 			});
-			Route::resource('/commandés', 'Admin\Commandes\Hna\CommandesController');
-			Route::resource('/livrés', 'Admin\Commandes\Hna\LivresController');
-			Route::resource('/rejetés', 'Admin\Commandes\Hna\RejetesController', 
+			Route::resource('/vitrine', 'Admin\Cmh\VitrineController', 
 				['names' => [
-						'index' => 'rejetéshna.index',
-						'create' => 'rejetéshna.create',
-						'store' => 'rejetéshna.store',
-						'show' => 'rejetéshna.show',
-						'edit' => 'rejetéshna.edit',
-						'update' => 'rejetéshna.update',
-						'destroy' => 'rejetéshna.destroy'
+						'index' => 'vitrinecmh.index',
+						'create' => 'vitrinecmh.create',
+						'store' => 'vitrinecmh.store',
+						'show' => 'vitrinecmh.show',
+						'edit' => 'vitrinecmh.edit',
+						'update' => 'vitrinecmh.update',
+						'destroy' => 'vitrinecmh.destroy'
 					]
-				]);		
+				]);
+			Route::resource('/stock', 'Admin\Cmh\StockController');
+			Route::resource('/ajout-produit', 'Admin\Cmh\AjoutProduitController');
 		});
-	});
-	//Haouta Routes
-	Route::group(['prefix' => 'haouta'], function() {
-		Route::get('/', function() {
-			return redirect()->route('vitrinehaouta.index');
+		//Users Routes
+		Route::group(['prefix' => 'utilisateurs'], function() {
+			Route::get('/', function() {
+				return redirect()->route('comptes.index');
+			});
+			Route::resource('/comptes', 'Admin\Utilisateurs\ComptesController');
+			Route::resource('/plaintes', 'Admin\Utilisateurs\PlaintesController');
 		});
-		Route::resource('/vitrine', 'Admin\Haouta\VitrineController', 
-			['names' => [
-					'index' => 'vitrinehaouta.index',
-					'create' => 'vitrinehaouta.create',
-					'store' => 'vitrinehaouta.store',
-					'show' => 'vitrinehaouta.show',
-					'edit' => 'vitrinehaouta.edit',
-					'update' => 'vitrinehaouta.update',
-					'destroy' => 'vitrinehaouta.destroy'
-				]
-			]);
-		Route::resource('/en-attentes', 'Admin\Haouta\EnAttentesController');
-		Route::resource('/ajout-lien', 'Admin\Haouta\AjoutLienController');
-	});
-	//CMH Routes
-	Route::group(['prefix' => 'cmh'], function() {
-		Route::get('/', function() {
-			return redirect()->route('vitrinecmh.index');
-		});
-		Route::resource('/vitrine', 'Admin\Cmh\VitrineController', 
-			['names' => [
-					'index' => 'vitrinecmh.index',
-					'create' => 'vitrinecmh.create',
-					'store' => 'vitrinecmh.store',
-					'show' => 'vitrinecmh.show',
-					'edit' => 'vitrinecmh.edit',
-					'update' => 'vitrinecmh.update',
-					'destroy' => 'vitrinecmh.destroy'
-				]
-			]);
-		Route::resource('/stock', 'Admin\Cmh\StockController');
-		Route::resource('/ajout-produit', 'Admin\Cmh\AjoutProduitController');
-	});
-	//Users Routes
-	Route::group(['prefix' => 'utilisateurs'], function() {
-		Route::get('/', function() {
-			return redirect()->route('comptes.index');
-		});
-		Route::resource('/comptes', 'Admin\Utilisateurs\ComptesController');
-		Route::resource('/plaintes', 'Admin\Utilisateurs\PlaintesController');
 	});
 });
 
@@ -119,6 +127,7 @@ Route::get('/externalproduct', function() {
 Route::get('/adminexternalproduct', function(){
 	return view('layouts.adminexternalproduct');
 });
-Auth::routes();
 
+// User Auth routes
+Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
