fdf1406dbde8081709dcba9627235d6bc76b3b68
Youki Sat <deathyouki@gmail.com>
2017-06-14 10:29:23 +0000

    work in progress

--- resources/views/layouts/Admin/Administration.blade.php
+++ resources/views/layouts/Admin/Administration.blade.php
@@ -5,5 +5,24 @@
 @endsection
 
 @section('content')
-	
+	<div id="j-haouta-en-attente">
+	<div id="j-product-script-injection"></div>
+	<section class="col-md-12" id="j-product-section" style="display: none;">
+		<div class="col-md-6">
+			<div class="main-image" id="j-product-image-main"></div>
+			<div class="thumbnails" id="j-product-thumbnails"></div>
+		</div>
+		
+		<div class="col-md-6">
+			<div class="informations">
+				<div class="informations__info" id="j-product-name"></div>
+
+				<div class="clearfix"></div>
+				<label class="informations__label informations__label--hidden" id="j-product-chosenvariants-label">Variantes : </label>
+				<div class="informations__info" id="j-product-variants"></div>
+
+			</div>
+		</div>
+	</section class="col-md-12">
+	</div>
 @endsection
